512f1fd2b4cf0631896738744d42d7676dd39413
Andreas Ziegler <andyziegler@gmx.de>
2022-05-24 18:53:27 +0200

    Database Project + Docker compose

---
+++ sys-src/images/database/src/database-service/TwitterTrend.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class TwitterTrend
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ID { get; set; }
+
+    [BsonRepresentation(BsonType.DateTime)]
+    public DateTime DateTime { get; set; }
+    public Dictionary<string, int> Trends { get; set; }
+}
