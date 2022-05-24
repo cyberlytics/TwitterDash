955cff1bf50463e04905e738de87e7c0af164d5d
Andreas Ziegler <a.ziegler1@oth-aw.de>
2022-05-24 18:53:27 +0200

    Database Project + Docker compose
    Former-commit-id: cbc29c304c6e19039773e3fd9e8286e6bd930898

---
+++ sys-src/images/database/src/database-service/Program.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+MongoClient client = new MongoClient($@"mongodb://root:example@mongo:27017/");
+
+var database = client.GetDatabase("TwitterDash");
+var collection = database.GetCollection<TwitterTrend>("Trends");
+
+await collection.InsertOneAsync(new()
+{
+    DateTime = DateTime.Now,
+    Trends = new()
+    {
+        { "#GRPC", 1 },
+        { "#Donald Trump", 2 },
+    }
+});
+
+var results = await collection.FindAsync(_ => true);
+Console.WriteLine(results);
