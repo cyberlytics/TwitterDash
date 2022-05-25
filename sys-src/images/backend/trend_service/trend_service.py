983357994f29c77ef2e25c542ada529fd02fd9ba
Felix Paris <f.paris1@oth-aw.de>
2022-05-25 19:02:30 +0200

    adjust getTrending for gRPC call
    additionally outsource twitter api tokens in json file

--- sys-src/images/backend/trend_service/trend_service.py
+++ sys-src/images/backend/trend_service/trend_service.py
@@ -1,36 +1,70 @@
 from stub.TrendService_pb2_grpc import TrendProviderServicer
-from stub.objects_pb2 import  TrendProviderReply,Trend,TrendType
+from stub.objects_pb2 import TrendProviderReply, Trend, TrendType
 import stub.objects_pb2
 import stub.TrendService_pb2_grpc
-import grpc 
+import grpc
 from concurrent import futures
 import time
+from twitter_api_caller import Twitter_API_Caller
+import json
+
 
 class TrendService(TrendProviderServicer):
-    
     def __init__(self) -> None:
         super().__init__()
         self.isActive = True
-        
-    #see https://github.com/melledijkstra/python-grpc-chat for "Event-System"
-    def OnNewTrends(self, request:stub.TrendService_pb2_grpc.google_dot_protobuf_dot_empty__pb2.Empty, context)->TrendProviderReply:
+
+        with open(
+            "./sys-src/images/backend/trend_service/tokens.json", "r"
+        ) as token_file:
+            tokens = json.load(token_file)
+
+        self.caller = Twitter_API_Caller(
+            tokens["consumer_key_v1"],
+            tokens["consumer_secret_v1"],
+            tokens["access_token_v1"],
+            tokens["access_token_secret_v1"],
+            tokens["consumer_key_v2"],
+            tokens["consumer_secret_v2"],
+            tokens["access_token_v2"],
+            tokens["access_token_secret_v2"],
+            tokens["bearer_token_v2"],
+        )
+
+    # see https://github.com/melledijkstra/python-grpc-chat for "Event-System"
+    def OnNewTrends(
+        self,
+        request: stub.TrendService_pb2_grpc.google_dot_protobuf_dot_empty__pb2.Empty,
+        context,
+    ) -> TrendProviderReply:
         print(f"Connection from {context.peer()}")
         while self.isActive:
             reply = TrendProviderReply()
             trends = []
-            for i in range(3):
-                trends.append(Trend(name="test",country=i,placement = i, tweetVolume24 = i,trendType = TrendType.Hashtag ))
-             
+
+            # Get the trending hashtags
+            for trend in self.caller.getTrending():
+                trends.append(
+                    Trend(
+                        name=trend["hashtag"],
+                        country=trend["country"],
+                        placement=trend["top"],
+                        tweetVolume24=trend["tweet_volume"],
+                        trendType=TrendType.Hashtag,
+                    )
+                )
+
             reply.timestamp.GetCurrentTime()
             reply.trends.extend(trends)
             yield reply
-            time.sleep(3)
+            time.sleep(60)
 
-   
-   
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-    stub.TrendService_pb2_grpc.add_TrendProviderServicer_to_server(TrendService(), server)
-    server.add_insecure_port('[::]:50051')
+    stub.TrendService_pb2_grpc.add_TrendProviderServicer_to_server(
+        TrendService(), server
+    )
+    server.add_insecure_port("127.0.0.1:50051")
     server.start()
     server.wait_for_termination()
--- sys-src/proto/compile_proto.py
+++ sys-src/proto/compile_proto.py
@@ -1,79 +1,109 @@
 ###
-# Compiles the Protobuff and GRPC Stubs for  all services   
+# Compiles the Protobuff and GRPC Stubs for  all services
 ###
 
-import os 
+import os
 import subprocess
 import shutil
 import json
 
+SRCDIR = "sys-src"
 PROTODIR = "proto"
 
 DEPENDENCIES = ["objects.proto"]
 
 # Pairs the Service to the Target Directory
-with open(os.path.join(os.getcwd(),PROTODIR,"ServiceMap.json")) as json_file:
+with open(os.path.join(os.getcwd(), SRCDIR, PROTODIR, "ServiceMap.json")) as json_file:
     SERVICES = json.load(json_file)
-    
+
 if __name__ == "__main__":
-    
+
     try:
-        subprocess.call(" ".join(["python","-m", "grpc_tools.protoc"]), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.call(
+            " ".join(["python", "-m", "grpc_tools.protoc"]),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
     except:
-        print("The python grpc_tools.protoc module is not installed. Please run 'pip install grpcio-tools' and try again!")
-    
-    protodir =  f'\"{os.path.join(os.getcwd(),PROTODIR)}\"'
-    
+        print(
+            "The python grpc_tools.protoc module is not installed. Please run 'pip install grpcio-tools' and try again!"
+        )
+
+    protodir = f'"{os.path.join(os.getcwd(),SRCDIR,PROTODIR)}"'
+
     for service in SERVICES:
-        outdir = os.path.join(os.getcwd(),SERVICES[service])
-        #Check if the Directory Exists
+        outdir = os.path.join(os.getcwd(), SRCDIR, SERVICES[service])
+        # Check if the Directory Exists
         if os.path.isdir(outdir):
-            #Remove it
+            # Remove it
             shutil.rmtree(outdir)
-        #Create the Directory
+        # Create the Directory
         os.makedirs(outdir)
-        with open(os.path.join(outdir,"__init__.py"),"w") as init_file:
+        with open(os.path.join(outdir, "__init__.py"), "w") as init_file:
             pass
-        outdir = f'\"{outdir}\"'
+        outdir = f'"{outdir}"'
         # Compile the Proto File
         try:
             service_name = service.split(".")[0]
-            grpc_file = os.path.join(SERVICES[service],f"{service_name}_pb2_grpc.py")
-            proto_file = os.path.join(SERVICES[service],f"{service_name}_pb2.py")
-            
+            grpc_file = os.path.join(SERVICES[service], f"{service_name}_pb2_grpc.py")
+            proto_file = os.path.join(SERVICES[service], f"{service_name}_pb2.py")
+
             generated_dependencies = []
-            #Generate Dependencies
+            # Generate Dependencies
             for dependency in DEPENDENCIES:
-                args = ["python","-m", "grpc_tools.protoc", "-I", protodir, f'--python_out={outdir}', dependency]
+                args = [
+                    "python",
+                    "-m",
+                    "grpc_tools.protoc",
+                    "-I",
+                    protodir,
+                    f"--python_out={outdir}",
+                    dependency,
+                ]
                 generated_dependencies.append(f"{dependency.split('.')[0]}_pb2")
                 subprocess.call(" ".join(args))
-                
-            args = ["python","-m", "grpc_tools.protoc", "-I", protodir, f'--python_out={outdir}',  f'--grpc_python_out={outdir}', service]
+
+            args = [
+                "python",
+                "-m",
+                "grpc_tools.protoc",
+                "-I",
+                protodir,
+                f"--python_out={outdir}",
+                f"--grpc_python_out={outdir}",
+                service,
+            ]
             subprocess.call(" ".join(args))
 
-            #Adjust the imports to the current location
+            # Adjust the imports to the current location
             def replace_import(file, old_import, new_import):
                 lines = []
-                with open (file, "r") as f:
+                with open(file, "r") as f:
                     lines = f.readlines()
-                    
-                for i,line in enumerate(lines):
+
+                for i, line in enumerate(lines):
                     if old_import in line:
                         lines[i] = line.replace(old_import, new_import)
-                        
-                with open (file, "w") as f:
+
+                with open(file, "w") as f:
                     f.writelines(lines)
-                        
+
             for generated_dependency in generated_dependencies:
-                cleaned_outdir = outdir.replace('"','')
+                cleaned_outdir = outdir.replace('"', "")
                 base_import_path = os.path.basename(os.path.normpath(outdir))
-                replace_import(os.path.join(cleaned_outdir,f"{service_name}_pb2_grpc.py"), f"import {generated_dependency} as", f"import {base_import_path[:-1]}.{generated_dependency} as")
-                replace_import(os.path.join(cleaned_outdir,f"{service_name}_pb2.py"), f"import {generated_dependency} as", f"import {base_import_path[:-1]}.{generated_dependency} as")
-                
+                replace_import(
+                    os.path.join(cleaned_outdir, f"{service_name}_pb2_grpc.py"),
+                    f"import {generated_dependency} as",
+                    f"import {base_import_path[:-1]}.{generated_dependency} as",
+                )
+                replace_import(
+                    os.path.join(cleaned_outdir, f"{service_name}_pb2.py"),
+                    f"import {generated_dependency} as",
+                    f"import {base_import_path[:-1]}.{generated_dependency} as",
+                )
+
             print(f"Created '{grpc_file}' and '{proto_file}'.")
-            
-            
-            
+
         except Exception as e:
             print(f"Failed to create '{grpc_file}' and '{proto_file}'!")
             print(e)
