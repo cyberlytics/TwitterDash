8b29e1628ad59c6791c4cfcd45e737c95724b689
Andreas Ziegler <a.ziegler1@oth-aw.de>
2022-05-31 11:34:52 +0200

    db grpc writer
    Former-commit-id: 99e928769fecbe894cca9d726a6881c6ad4ff01e

--- sys-src/images/database/src/database-service/Program.cs
+++ sys-src/images/database/src/database-service/Program.cs
@@ -1,29 +1,66 @@
-﻿using MongoDB.Driver;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using System.Net;
 
 var mongodb_port = Environment.GetEnvironmentVariable("MONGODB_PORT") ?? "27017";
 var mongodb_ip = Environment.GetEnvironmentVariable("MONGODB_IP") ?? "localhost";
 var mongodb_user = Environment.GetEnvironmentVariable("MONGODB_USER") ?? "root";
 var mongodb_password = Environment.GetEnvironmentVariable("MONGODB_PASSWORD") ?? "example";
+var grpc_port = int.Parse(Environment.GetEnvironmentVariable("GRPC_PORT") ?? "50051");
 
-MongoClient client = new MongoClient($@"mongodb://{mongodb_user}:{mongodb_password}@{mongodb_ip}:{mongodb_port}/");
+var builder = WebApplication.CreateBuilder(args);
 
-var database = client.GetDatabase("TwitterDash");
-var collection = database.GetCollection<TwitterTrend>("Trends");
-
-while (true)
+builder.WebHost.ConfigureKestrel(options =>
 {
-    Console.WriteLine("Creating Entry");
-    await collection.InsertOneAsync(new()
+    options.ConfigureHttpsDefaults(httpsOptions =>
     {
-        DateTime = DateTime.Now,
-        Trends = new()
-        {
-            { "#GRPC", 1 },
-            { "#Donald Trump", 2 },
-        }
+        httpsOptions.SslProtocols = System.Security.Authentication.SslProtocols.Tls12;
     });
-    Task.Delay(3000).Wait();
-}
+    options.Listen(IPAddress.Any, grpc_port, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
 
-var results = await collection.FindAsync(_ => true);
-Console.WriteLine(results);
+builder.Services.AddGrpc();
+
+builder.Services.AddTransient<MongoClient>((_) => new MongoClient($@"mongodb://{mongodb_user}:{mongodb_password}@{mongodb_ip}:{mongodb_port}/"));
+
+var app = builder.Build();
+
+app.UseRouting();
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    app.MapGrpcService<DatabaseWriterController>();
+    app.MapGrpcService<DatabaseReaderController>();
+});
+
+app.Run();
+
+
+//var database = client.GetDatabase("TwitterDash");
+//var collection = database.GetCollection<TwitterTrend>("Trends");
+
+//while (true)
+//{
+//    Console.WriteLine("Creating Entry");
+//    await collection.InsertOneAsync(new()
+//    {
+//        DateTime = DateTime.Now,
+//        Trends = new()
+//        {
+//            { "#GRPC", 1 },
+//            { "#Donald Trump", 2 },
+//        }
+//    });
+//    Task.Delay(3000).Wait();
+//}
+
+//var results = await collection.FindAsync(_ => true);
+//Console.WriteLine(results);
--- sys-src/images/database/src/database-service/TwitterTrend.cs
+++ sys-src/images/database/src/database-service/TwitterTrend.cs
@@ -1,13 +1,28 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-public class TwitterTrend
-{
+public class TwitterTrends
+{    
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string ID { get; set; }
 
     [BsonRepresentation(BsonType.DateTime)]
     public DateTime DateTime { get; set; }
-    public Dictionary<string, int> Trends { get; set; }
+    public List<TwitterTrend> Trends { get; set; }
+}
+
+public class TwitterTrend
+{
+    public TrendType trendType;
+    public string name;
+    public int woeid;
+    public int placement;
+    public int tweetVolume24;
+
+}
+public enum TrendType
+{
+    Topic,
+    Hashtag
 }
