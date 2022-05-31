a3c353160548d76e132bd301689b8d6232a1b8b5
Andreas Ziegler <a.ziegler1@oth-aw.de>
2022-05-31 11:34:52 +0200

    db grpc writer

---
+++ sys-src/images/database/src/database-service/DatabaseReaderController.cs
@@ -0,0 +1,30 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Twitterdash;
+using MongoDB.Driver;
+
+internal class DatabaseReaderController : Twitterdash.DatabaseReader.DatabaseReaderBase
+{
+    
+    private MongoClient client;
+
+    public DatabaseReaderController(MongoClient client)
+    {
+        this.client = client;
+    }
+
+    public override Task<GetAvailableCountriesReply> GetAvailableCountries(Empty request, ServerCallContext context)
+    {
+        return base.GetAvailableCountries(request, context);
+    }
+
+    public override Task<TrendProviderReply> GetCurrentTrends(GetCurrentTrendsRequest request, ServerCallContext context)
+    {
+        return base.GetCurrentTrends(request, context);
+    }
+
+    public override Task<GetRecentTrendsReply> GetRecentTrends(GetRecentTrendsRequest request, ServerCallContext context)
+    {
+        return base.GetRecentTrends(request, context);
+    }
+}
---
+++ sys-src/images/database/src/database-service/DatabaseWriterController.cs
@@ -0,0 +1,45 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Twitterdash;
+using MongoDB.Driver;
+
+internal class DatabaseWriterController : Twitterdash.DatabaseWriter.DatabaseWriterBase
+{
+
+    private MongoClient client;
+
+    public DatabaseWriterController(MongoClient client)
+    {
+        this.client = client;
+    }
+
+    public override async Task<Empty> StoreTrends(TrendProviderReply request, ServerCallContext context)
+    {        
+        var database = client.GetDatabase("TwitterDash");
+        var collection = database.GetCollection<TwitterTrends>("Trends");
+
+        var timestamp = request.Timestamp.ToDateTime();
+
+        var Trends = new List<TwitterTrend>();
+        foreach (var trend in request.Trends)
+        {
+            var twitterTrend = new TwitterTrend();
+            twitterTrend.trendType = (TrendType)trend.TrendType;
+            twitterTrend.name = trend.Name;
+            twitterTrend.woeid = trend.Country;
+            twitterTrend.placement = trend.Placement;
+            twitterTrend.tweetVolume24 = trend.TweetVolume24;
+
+
+            Trends.Add(twitterTrend);
+        }
+
+        await collection.InsertOneAsync(new()
+        {
+            DateTime = timestamp,
+            Trends = Trends
+        });
+
+        return new Empty();
+    }
+}
