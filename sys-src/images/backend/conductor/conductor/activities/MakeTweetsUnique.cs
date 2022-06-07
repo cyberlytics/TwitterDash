0c1b174a16bb86171f698cb8ddeaa870508f9a0a
Lukas Kreussel <l.kreussel@oth-aw.de>
2022-06-07 17:09:32 +0200

    Added Activities and Unit-Tests
    Former-commit-id: 187da17d878cd96437c0cf819564bf991633a000

---
+++ sys-src/images/backend/conductor/conductor/activities/MakeTweetsUnique.cs
@@ -0,0 +1,22 @@
+﻿using Elsa.ActivityResults;
+using Elsa.Services;
+using Elsa.Services.Models;
+
+namespace conductor.activities
+{
+    public class MakeTweetsUnique : Activity
+    {
+
+
+        public MakeTweetsUnique()
+        {
+            
+        }
+
+
+        protected override async ValueTask<IActivityExecutionResult> OnExecuteAsync(ActivityExecutionContext context)
+        {
+            return Outcome(Nameservice.Outcomes.Done);
+        }
+    }
+}
--- sys-src/images/backend/conductor/conductor/workflows/TrendWorkflow.cs
+++ sys-src/images/backend/conductor/conductor/workflows/TrendWorkflow.cs
@@ -19,18 +19,52 @@ namespace conductor.workflows
                 .WithDescription("Validate the Trends recieved from the Trend-Service")
                 .WithId(nameof(ValidateTrends))
                 .WithDisplayName("Validate Trends")
-                .ParallelForEach(new string[] { "Persist", "Gather Tweets" }, iterate =>
-                  iterate.If(ctx => (string)ctx.Input == "Persist")
-                      .Then<PersistTrends>()
-                      .WithDescription("Persist the trends to a Database")
-                      .WithId(nameof(PersistTrends))
-                      .WithDisplayName("Save Trends")
-                      .ThenNamed("Wait")
+                .Then<Fork>(fork => fork.WithBranches("Persist", "Gather Tweets"), fork =>
+                {
+                    fork
+                    .When("Persist")
+                        .Then<PersistTrends>()
+                        .WithDescription("Persist the trends to a Database")
+                        .WithId(nameof(PersistTrends))
+                        .WithDisplayName("Save Trends")
+                    .ThenNamed("JoinTasks");
 
+                    fork
+                    .When("Gather Tweets")
+                        .Then<CollectTweets>()
+                        .WithDescription("Collect Tweets via Web-Scrapers")
+                        .WithId(nameof(CollectTweets))
+                        .WithDisplayName("Collect Tweets")
+                    .Then<MakeTweetsUnique>()
+                        .WithDescription("Compare the Tweet-IDs with the IDs already in the Database")
+                        .WithId(nameof(MakeTweetsUnique))
+                        .WithDisplayName("Filter Tweets")
 
-                );
-                //.Then<CollectTweets>()
+                    .Then<Fork>(analysisfork => analysisfork.WithBranches("Sentiment", "TopicModeling"), analysisfork =>
+                    {
+                        analysisfork
+                        .When("Sentiment")
+                            .Then<PerformSentimentAnalysis>()
+                            .Then<PersistSentiment>()
+                            .ThenNamed("JoinAnalysisTasks");
 
+                        analysisfork
+                        .When("TopicModeling")
+                            .Then<PerformTopicModeling>()
+                            .Then<PersistTopicModels>()
+                            .ThenNamed("JoinAnalysisTasks");
+
+                    });
+                    
+
+                    fork.Add<Join>(x => x.WithMode(Join.JoinMode.WaitAll))
+                   .WithDisplayName("Join Tasks")
+                   .WithName("JoinAnalysisTasks")
+                   .ThenNamed("JoinTasks");
+                })
+                .Add<Join>(x => x.WithMode(Join.JoinMode.WaitAll))
+                .WithDisplayName("Join Tasks")
+                .WithName("JoinTasks");
         }
     }
 }
