362c72dcfdafd09fa8061b7dd40b5939baa30cc0
Lukas Kreussel <l.kreussel@oth-aw.de>
2022-06-07 11:14:43 +0200

    Added Definition for Tweet and Sentiment Service
    Co-authored-by: Bastian Hahn <b.hahn@oth-aw.de>
    
    Former-commit-id: b4fd902d041b7f40020a0d0e4c66813c928c081c

--- sys-src/images/backend/conductor/conductor/workflows/TrendWorkflow.cs
+++ sys-src/images/backend/conductor/conductor/workflows/TrendWorkflow.cs
@@ -16,8 +16,21 @@ namespace conductor.workflows
             builder
                 .WithDisplayName("Trend Workflow")
                 .Then<ValidateTrends>()
+                .WithDescription("Validate the Trends recieved from the Trend-Service")
+                .WithId(nameof(ValidateTrends))
+                .WithDisplayName("Validate Trends")
+                .ParallelForEach(new string[] { "Persist", "Gather Tweets" }, iterate =>
+                  iterate.If(ctx => (string)ctx.Input == "Persist")
+                      .Then<PersistTrends>()
+                      .WithDescription("Persist the trends to a Database")
+                      .WithId(nameof(PersistTrends))
+                      .WithDisplayName("Save Trends")
+                      .ThenNamed("Wait")
+
+
+                );
                 //.Then<CollectTweets>()
-                .Then<PersistTrends>();
+
         }
     }
 }
