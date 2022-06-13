16220dd1befe9b86ac13a2b611dfc1a7f976be18
Bastian Hahn <b.hahn@oth-aw.de>
2022-06-13 19:05:01 +0200

    Sentiment Service Docker created

--- sys-src/images/backend/sentiment_service/sentiment_service_client.py
+++ sys-src/images/backend/sentiment_service/sentiment_service_client.py
@@ -10,10 +10,11 @@ import datetime
 
 
 if __name__ == "__main__":
-    channel = grpc.insecure_channel('127.0.0.1:50014')
+    channel = grpc.insecure_channel('127.0.0.1:50050')
     client = SentimentProviderStub(channel)
     
-    request = GetSentimentRequest(text="Ich liebe mein neues Auto.")
+    #request = GetSentimentRequest(text="Ich liebe mein neues Auto.")
+    request = GetSentimentRequest(text="Ich hasse mein neues Auto.")
 
     response = client.GetSentiment(request)
 
