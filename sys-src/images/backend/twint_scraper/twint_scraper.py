7fc8d2fb3082b35a4d76d226ea31977798db9c8c
Bastian Hahn <b.hahn@oth-aw.de>
2022-06-14 21:04:07 +0200

    Update twint_scraper.py
    Co-authored-by: Felix Paris <f.paris1@oth-aw.de>
    
    Former-commit-id: 658745366e9b171ade7a0ee4ec1b9cd6a60609ab

--- sys-src/images/backend/twint_scraper/twint_scraper.py
+++ sys-src/images/backend/twint_scraper/twint_scraper.py
@@ -5,24 +5,22 @@ from ast import literal_eval
 import re
 
 
-class Twint_Scraper():
+class Twint_Scraper:
     def __init__(self):
         self.df = None
-        
+
         nest_asyncio.apply()
-     
-    def extract_reply_to(self, stringliste):
-        if len(stringliste) > 10:
-            mentionsliste = []
-            liste = literal_eval(stringliste)
-            for mention in liste:
-                mentionsliste.append(mention["screen_name"])
-            return mentionsliste
-        else:
-            return stringliste
-     
-        
-    def searchTwint(self, keyword, since=None, until=None, language=None, limit=None, createDataFrame=True, hide_out=True):
+
+    def searchTwint(
+        self,
+        keyword,
+        since=None,
+        until=None,
+        language=None,
+        limit=None,
+        createDataFrame=True,
+        hide_out=True,
+    ):
         c = twint.Config()
         c.Search = "{} lang:{}".format(keyword, language)
         c.Filter_retweets = False
@@ -42,20 +40,42 @@ class Twint_Scraper():
             self.df = twint.storage.panda.Tweets_df
             self.df = self.dataframe_cleanup(self.df)
             return self.toTweet(self.df)
-        
+
     def dataframe_cleanup(self, df):
         df = df.drop_duplicates(subset=["id"])
-        df.drop(columns=["created_at", "place", "cashtags", "user_id_str", "day", "hour", "urls", "thumbnail", "quote_url", "near", "geo", "source", "user_rt_id", "user_rt", "retweet_id", "retweet", "retweet_date", "translate", "trans_src", "trans_dest"], inplace=True)
+        df.drop(
+            columns=[
+                "created_at",
+                "place",
+                "cashtags",
+                "user_id_str",
+                "day",
+                "hour",
+                "urls",
+                "thumbnail",
+                "quote_url",
+                "near",
+                "geo",
+                "source",
+                "user_rt_id",
+                "user_rt",
+                "retweet_id",
+                "retweet",
+                "retweet_date",
+                "translate",
+                "trans_src",
+                "trans_dest",
+            ],
+            inplace=True,
+        )
         df = df[df["language"] == "de"]
         df["date"] = df["date"].astype("datetime64")
-        #df["created_at"] = df["created_at"].astype("datetime64")
         df["Wochentag"] = df["date"].apply(lambda x: x.weekday())
         df["Stunde"] = df["date"].apply(lambda x: x.hour)
         df["Originale_Tweetlaenge"] = df["tweet"].apply(lambda x: len(x))
-        #df["reply_to"] = df["reply_to"].apply(lambda x: str(self.extract_reply_to(x)))
         df["date"] = df["date"].astype(str)
         df["language"] = df["language"].astype(str)
-        
+
         return df
 
     def toTweet(self, df):
@@ -72,23 +92,12 @@ class Twint_Scraper():
                     "nreplies": row["nreplies"],
                     "nretweets": row["nretweets"],
                     "hashtags": row["hashtags"],
-                    "language": row["language"]
+                    "language": row["language"],
                 }
             )
         return tweets
-        
-    # Data Analysis
-    # TODO  
-    
-    
-    # Sentiment Analysis
-    # TODO  
-    def sentiment_analysis(self, df):
-        pass
-    
-    # save in Database
-    # TODO  
-    
+
+
 # if __name__ == "__main__":
 #     twint_scraper = Twint_Scraper()
 #     testdf = twint_scraper.searchTwint("corona", since="2022-05-18", until="2022-05-19", language="de", limit=500, createDataFrame=True)
