2c3e3c0d0c98535061f9c172e9b6f55740bcbb28
Klier, Andreas <a.klier@oth-aw.de>
2022-06-14 10:29:10 +0200

    fix reads not working

--- sys-src/images/database/src/database-service/ITwitterTrendsRepository.cs
+++ sys-src/images/database/src/database-service/ITwitterTrendsRepository.cs
@@ -5,5 +5,5 @@ public interface ITwitterTrendsRepository
     Task StoreTrends(TwitterTrends trends);
     Task<List<int>> GetAvailableCountries();
     Task<TwitterTrends> GetCurrentTrends(int woeid);
-    Task<List<TwitterTrends>> GetRecentTrends(DateTime start, DateTime end, string hashtag);
+    Task<List<TwitterTrends>> GetRecentTrends(DateTime? start, DateTime? end, string hashtag);
 }
